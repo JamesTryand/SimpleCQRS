197b002ca0424f9c3bb6790c9f62550f843d1aba
James Tryand <james.tryand@gmail.com>
2012-11-18 19:34:52 +0000

    adding the stubs to the methods for the redis eventstores

--- src/SimpleCqrs.EventStore.Redis/RedisEventStore.cs
+++ src/SimpleCqrs.EventStore.Redis/RedisEventStore.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SimpleCqrs.Eventing;
 
 namespace SimpleCqrs.EventStore.Redis
 {
-    class RedisEventStore
+    public class RedisEventStore : IEventStore
     {
+        public IEnumerable<DomainEvent> GetEvents(Guid aggregateRootId, int startSequence)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(IEnumerable<DomainEvent> domainEvents)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<DomainEvent> GetEventsByEventTypes(IEnumerable<Type> domainEventTypes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<DomainEvent> GetEventsByEventTypes(IEnumerable<Type> domainEventTypes, Guid aggregateRootId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<DomainEvent> GetEventsByEventTypes(IEnumerable<Type> domainEventTypes, DateTime startDate, DateTime endDate)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- src/SimpleCqrs.EventStore.Redis/RedisSnapshotStore.cs
+++ src/SimpleCqrs.EventStore.Redis/RedisSnapshotStore.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SimpleCqrs.Eventing;
 
 namespace SimpleCqrs.EventStore.Redis
 {
-    class RedisSnapshotStore
+    public class RedisSnapshotStore : ISnapshotStore
     {
+        public Domain.Snapshot GetSnapshot(Guid aggregateRootId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveSnapshot<TSnapshot>(TSnapshot snapshot) where TSnapshot : Domain.Snapshot
+        {
+            throw new NotImplementedException();
+        }
     }
 }
